22704d3cc19e7f8abf7c0af62258e528802bfa3b
gmustafayeva <gmustafayeva@student.sierracollege.edu>
2024-11-17 20:11:48 -0800

    Updated crack.c file

--- md5.c
+++ md5.c
@@ -9,9 +9,11 @@ char *md5(const char *str, int length) {
     uint8_t md5_digest[md5_digest_len];
     char *hexdigest = (char*) malloc(md5_digest_len * 2 + 1);
 
+    // MD5_Init
     mdctx = EVP_MD_CTX_new();
     EVP_DigestInit_ex(mdctx, EVP_md5(), NULL);
 
+    // MD5_Update
     while (length > 0) {
         if (length > 512) {
             EVP_DigestUpdate(mdctx, str, 512);
@@ -22,11 +24,12 @@ char *md5(const char *str, int length) {
         str += 512;
     }
 
+    // MD5_Final
     EVP_DigestFinal_ex(mdctx, md5_digest, &md5_digest_len);
     EVP_MD_CTX_free(mdctx);
 
     for (int n = 0; n < md5_digest_len; ++n) {
-        snprintf(hexdigest + n * 2, 3, "%02x", md5_digest[n]);
+        snprintf(hexdigest + n*2, 3, "%02x", md5_digest[n]);
     }
 
     hexdigest[md5_digest_len * 2] = '\0';
